8ab076533a4b7b14ceaa4e830693326816ab366b
TheRealH0u <aplazl55@gmail.com>
2021-02-18 12:33:28 +0100

    a

--- routes/index.js
+++ routes/index.js
@@ -38,7 +38,7 @@ router.get('/hi', (req, res) => {
 let brackets = {
     lol: {
         game: 'League of Legends',
-        bracket: 'https://challonge.com/xcn1tefm/module'
+        bracket: 'https://battlefy.com/embeds/602e49d11f489f7f47cd3b34/stage/602e4a7fa74cc17c31b7e179'
     },
     valorant: {
         game: 'Valorant',
